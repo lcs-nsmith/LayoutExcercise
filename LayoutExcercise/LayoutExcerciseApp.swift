c668005276e383d5beda30152e3091dc1769932b
Nathan Smith <nsmith@lcs.on.ca>
2021-11-05 09:14:09 -0400

    Initial Commit

---
+++ LayoutExcercise/LayoutExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExcerciseApp.swift
+//  LayoutExcercise
+//
+//  Created by Nate S on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
