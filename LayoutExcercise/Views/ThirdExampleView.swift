3a7b5c4072ad266f1f514f3c6511c03db7380b34
Nathan Smith <nsmith@lcs.on.ca>
2021-11-06 11:20:39 -0400

    Third View done

--- LayoutExcercise/Views/ThirdExampleView.swift
+++ LayoutExcercise/Views/ThirdExampleView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct ThirdExampleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+        Image("gate1")
+            .resizable()
+            .scaledToFit()
+            Divider()
+            ZStack {
+                Rectangle()
+                    .fill(Color.brown.opacity(0.8))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                HStack {
+                    VStack {
+                    Text("NEVADA")
+                            .font(.largeTitle)
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra massa massa ultricies. At consectetur lorem donec massa sapien faucibus. Enim nulla aliquet porttitor lacus. Quis vel eros donec ac odio tempor orci.")
+                            .padding(.leading)
+                    }
+                    VStack{
+                        Text("At consectetur lorem donec massa sapien faucibus. Tincidunt tortor aliquam nulla facilisi cras fermentum. ")
+                        Text("HEALTH")
+                            .font(.largeTitle)
+                        Image("building1")
+                            .resizable()
+                            .scaledToFit()
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                    }
+                }
+            }
+        }
     }
 }
 
